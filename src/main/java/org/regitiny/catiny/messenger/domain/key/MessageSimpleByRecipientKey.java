c2ee2a80b24476fc084f9aeb409a06bd626d1c40
yuvytung <yuvytung@gmail.com>
2020-09-21 18:14:56 +0700

    MessageSimpleByRecipient update

--- src/main/java/org/regitiny/catiny/messenger/domain/key/MessageSimpleByRecipientKey.java
+++ src/main/java/org/regitiny/catiny/messenger/domain/key/MessageSimpleByRecipientKey.java
@@ -2,8 +2,10 @@ package org.regitiny.catiny.messenger.domain.key;
 
 import lombok.*;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
@@ -11,10 +13,14 @@ import java.util.UUID;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@PrimaryKeyClass
 public class MessageSimpleByRecipientKey
 {
+  @NotNull
   @PrimaryKeyColumn(name = "recipientId", type = PrimaryKeyType.PARTITIONED)
   protected UUID recipientId;
+
+  @NotNull
   @PrimaryKeyColumn(name = "topicId", ordinal = 0)
   protected UUID topicId;
 }
